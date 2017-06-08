c116949ed6a14352f645da5e61a04c9233c3718a
Tom.Huang <hzlhu.dargon@gmail.com>
2017-06-08 12:15:16 +0800

    remove empty line

--- library/src/main/java/me/tom/jsbridgewebview/JsBridgeWebView.java
+++ library/src/main/java/me/tom/jsbridgewebview/JsBridgeWebView.java
@@ -86,7 +86,6 @@ public class JsBridgeWebView extends WebView {
                         }
                     }
                 });
-
             }
         }, "jsBridgeWebViewInterface");
     }
